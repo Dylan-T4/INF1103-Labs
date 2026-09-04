31df3158981feb87c1d2389312c93966571b009f
Dylan <2603841@sit.singaporetech.edu.sg>
2026-09-04 11:02:39 +0800

    Readded deleted activities :(

--- hello.py
+++ hello.py
@@ -1,3 +1,26 @@
+print("==========================")
+print("Welcome Here")
+print("My First Post!")
+print("==========================")
+
+username = "Big_Bob"
+bio = "Capturing The Pretty Sights Of The World"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
+
+followers = 100
+followers += 50
+print("Day 1: ", followers)
+
+followers += 20
+print("Day 2: ", followers)
+
+followers -= 10
+print("Day 3: ", followers)
+
 username = input("Enter Username: ")
 age = int(input("Enter Age: "))
 category = input("Enter Content Category: ")
