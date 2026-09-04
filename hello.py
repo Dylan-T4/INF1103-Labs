ac9e1a02af60f601a9d0667cf34df974f41409eb
Dylan <2603841@sit.singaporetech.edu.sg>
2026-09-04 10:41:03 +0800

    FirstFirst Post

--- hello.py
+++ hello.py
@@ -2,3 +2,11 @@ print("=============================")
 print("Welcome Here")
 print("My First Post!")
 print("=============================")
+
+username = "Big_Bob"
+bio = "Capturing The Pretty Sights Of The World"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
